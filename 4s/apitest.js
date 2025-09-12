66f6432155e38f46aae42976d633d64370ce3274
maintainx-maintops-sav <request.parts.maintainx@jcb.com>
2025-09-12 13:10:08 -0400

    Update apitest.js

--- 4s/apitest.js
+++ 4s/apitest.js
@@ -11,7 +11,7 @@ async function fetchMaintainXAssets() {
     const fullUrl = proxyUrl + encodeURIComponent(targetUrl);
 
     try {
-        const response = await fetch(fullUrl, {
+        const response = await fetch(targetUrl, {
             method: 'GET',
             headers: {
                 'Authorization': `Bearer ${apiKey}`,
